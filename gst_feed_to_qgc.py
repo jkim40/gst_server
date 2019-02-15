262f0bde5244c578d22ae653c75451041b4986ca
Hong Kim <htkim40@gmail.com>
2019-02-14 22:16:29 -0800

    Added method to parse video formats. Added pipeline for raw nv12 videos.

--- gst_feed_to_qgc.py
+++ gst_feed_to_qgc.py
@@ -23,6 +23,7 @@ import time
 import gi
 gi.require_version('Gst','1.0')
 from gi.repository import Gst, GObject
+import subprocess
 import os
 
 
@@ -41,7 +42,85 @@ class H264Pipeline:
         self.tee = None
         self.islinked = False
 
-    def gst_pipeline_h264_h264_init(self):
+    def gst_pipeline_raw_to_h264_init(self, vid_src = "/dev/video0"):
+        print("Initializing GST Pipeline")
+        Gst.init(None)
+
+        self.pipeline = Gst.Pipeline.new("raw to h.264")
+
+        # Initialize video feed sourcefrom
+        print("Initializing v4l2 source")
+        self.videosrc = Gst.ElementFactory.make("v4l2src","vid-src")
+        self.videosrc.set_property("device", vid_src)
+
+        # set up the Gst cap(s) for video/x-264 format
+        print("Generating video cap")
+        self.videocap = Gst.caps_from_string("video/x-raw,format=NV12,width=1920,height=1080,framerate=30/1")
+
+        # Initialize the video feed parser to parse h.264 frames
+        print("Initializing video parser")
+        self.videoparse = Gst.ElementFactory.make("rawvideoparse","vid-parse")
+
+        # Initialize the tee to split link into a file sink and a udp sink
+        print("Initializing tee")
+        self.tee = Gst.ElementFactory.make("tee","tee")
+
+        # Initialize file sink queue to be saved locally
+        print("Initializing fil sink queue")
+        self.filequeue = Gst.ElementFactory.make("queue", "file-queue")
+
+        # Initialize file sink
+        print("Initializing local file sink")
+        self.filesink = Gst.ElementFactory.make("filesink","file-sink")
+        self.filesink.set_property("location","/home/aero_%s"%(datetime.datetime.now().strftime("%y%m%d%H%M")))
+
+        # Initialize udp sink queue to be sent over udp/rtp
+        print("Initializing network queue")
+        self.networkqueue = Gst.ElementFactory.make("queue","network-queue")
+
+        # Initialize rtp encoder
+        print("Initializing rtp encoder")
+        self.rtpencoder = Gst.ElementFactory.make("rtph264pay", "rtp-enc")
+
+        # Initialize udp sink : requires ip address of qgc
+        print("Initializing udp sink")
+        self.udpsink = Gst.ElementFactory.make("udpsink","udp-sink")
+        self.udpsink.set_property("host","192.168.0.101")
+        self.udpsink.set_property("port",5600)
+
+        # Add all elements to the pipeline
+        print("Adding all elements to pipeline")
+        self.pipeline.add(self.videosrc)
+        self.pipeline.add(self.videoparse)
+        self.pipeline.add(self.tee)
+        self.pipeline.add(self.filequeue)
+        self.pipeline.add(self.filesink)
+        self.pipeline.add(self.networkqueue)
+        self.pipeline.add(self.rtpencoder)
+        self.pipeline.add(self.udpsink)
+
+        # Link elements together in order, with filter
+        print("Linking pipeline elements")
+
+        # Link elements before tee
+        self.videosrc.link_filtered(self.videoparse,self.videocap)
+        self.videoparse.link(self.tee)
+
+        # Link elements after tee regarding network
+        self.networkqueue.link(self.rtpencoder)
+        self.rtpencoder.link(self.udpsink)
+
+        # Link elements after tee regarding file sink
+        self.filequeue.link(self.filesink)
+
+        # Link tee to elements after tee
+        # self.tee.link(self.networkqueue)
+        self.tee.link(self.filequeue)
+        self.tee.link(self.networkqueue)
+
+        self.islinked = True
+
+    def gst_pipeline_h264_h264_init(self,vid_src = "/dev/video0"):
 
         print("Initializing GST Pipeline")
         Gst.init(None)
@@ -133,12 +212,18 @@ class H264Pipeline:
         print("Stopping video feed")
         self.pipeline.set_state(Gst.State.PAUSED)
 
-    def h264_to_h264_task(self):
-
+    def h264_to_h264_task(self, vid_src = "/dev/video0"):
+        get_video_formats()
         for d in query_video_devices():
             print(d)
+
+
         # Check compression formats for the video sources
-        # Prioritize the following : h264 mjpeg gray16le yuyv.
+        # Prioritize the following : h264 nv12 mjpeg yuyv.
+        # The reason for this prioritization is to put h264 with the best compression specs first, followed by what is
+        # known to be used only in the thermal cams that flightwave provides. It is, however, uncompressed. mjpeg be-
+        # cause it is compressed, though transcoding needs to occur. And then yuyv or yuv, which is raw. It is possibly
+        # better to take raw videos and directly compress instead of transcoding a compressed mjpeg.
         self.gst_pipeline_h264_h264_init()
         self.start_feed()
 
@@ -146,6 +231,12 @@ def query_video_devices():
     # query /dev/ for video sources. Returns list of video devices 
     device_path = "/dev" 
     return [f for f in os.listdir(device_path) if "video" in f]
+
+def get_video_formats(vid_src = "/dev/video0"):
+    video_formats_cmd = "ffmpeg -f v4l2 -list_formats all -i " + vid_src
+    p = subprocess.Popen(video_formats_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, err = p.communicate()
+    print(output)
 
 
 if __name__ == "__main__":
